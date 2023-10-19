961a19e5042ab0c55f378d23f51561aca31eb012
Jose Valenzuela <josecinuwu@gmail.com>
2023-10-18 19:13:34 -0700

    .

--- Clases/metodo.cs
+++ Clases/metodo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,58 +9,216 @@ namespace EjercicioBuscar_JSVE.Clases
 {
     internal class metodo
     {
-        public static int BuscarNumero(int[] array, int numero)
+        public void Actv1()
         {
-            for (int i = 0; i < array.Length; i++)
+            int[] num = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Ingrese el número a buscar:");
+            int NumeroBuscar = Convert.ToInt32(Console.ReadLine());
+
+            bool encontrado = false;
+
+            for (int i = 0; i < num.Length; i++)
             {
-                if (array[i] == numero)
+                if (NumeroBuscar == num[i])
                 {
-                    return i;
+                    Console.WriteLine($"El número {NumeroBuscar} se encuentra en la posición {i}.");
+                    encontrado = true;
+                    break;
                 }
             }
-            return -1;
+
+            if (!encontrado)
+            {
+                Console.WriteLine($"El número {NumeroBuscar} no se encuentra en la lista.");
+            }
         }
 
-        public static int BuscarCadena(string[] array, string cadena)
+        public void Actv2()
         {
-            for (int i = 0; i < array.Length; i++)
+            string[] cadena = { "Uno", "Dos", "Tres", "Cuatro", "Cinco" };
+
+            Console.WriteLine("A continuacion ingresaras números del Uno al Cinco pero con letra");
+            Console.WriteLine("Ingrese la cadena a buscar:");
+            string cadenaBuscar = Console.ReadLine();
+
+            bool encontrada = false;
+
+            for (int i = 0; i < cadena.Length; i++)
             {
-                if (array[i] == cadena)
+                if (cadenaBuscar == cadena[i])
                 {
-                    return i;
+                    Console.WriteLine($"La cadena '{cadenaBuscar}' se encuentra en la posición {i}.");
+                    encontrada = true;
+                    break;
                 }
             }
-            return -1;
+
+            if (!encontrada)
+            {
+                Console.WriteLine($"La cadena '{cadenaBuscar}' no se encuentra en la lista.");
+            }
+
+
         }
 
-        public static int EncontrarPrimerNumeroPar(int[] numeros)
+        public void Actv3()
         {
-            for (int i = 0; i < numeros.Length; i++)
+            int[] arreglo = { 2, 5, 7, 8, 12, 15, 21, 24 };
+
+            int posicionpar = -1;
+            for (int i = 0; i < arreglo.Length; i++)
             {
-                if (numeros[i] % 2 == 0)
+                if (arreglo[i] % 2 == 0)
                 {
-                    return i;
+                    posicionpar = i;
+                    break;
                 }
             }
-            return -1;
+
+            if (posicionpar != -1)
+            {
+                Console.WriteLine($"El primer número par se encuentra en la posición {posicionpar}.");
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron números pares en el arreglo.");
+            }
         }
 
-        public static void EncontrarNumerosPares(int[,] matriz)
+        public void Actv4()
         {
-            int filas = matriz.GetLength(0);
-            int columnas = matriz.GetLength(1);
+            int[] num = { 3, 7, 1, 4, 8, 2, 6, 9, 10, 4 };
 
             Console.WriteLine("Posiciones de números pares en la matriz:");
 
-            for (int i = 0; i < filas; i++)
+            for (int i = 0; i < num.Length; i++)
             {
-                for (int j = 0; j < columnas; j++)
+                if (num[i] % 2 == 0)
                 {
-                    if (matriz[i, j] % 2 == 0)
+                    Console.WriteLine($"Número par {num[i]} en la posición {i}.");
+                }
+            }
+        }
+
+        public void Actv5()
+        {
+            int[] orden = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("Poner el número a buscar:");
+            int numeroBuscar = Convert.ToInt32(Console.ReadLine());
+
+            int inicio = 0;
+            int fin = orden.Length - 1;
+            int posicion = -1;
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+
+                if (orden[medio] == numeroBuscar)
+                {
+                    posicion = medio;
+                    break;
+                }
+                else if (orden[medio] < numeroBuscar)
+                {
+                    inicio = medio + 1;
+                }
+                else
+                {
+                    fin = medio - 1;
+                }
+            }
+
+            if (posicion != -1)
+            {
+                Console.WriteLine($"El numero {numeroBuscar} se encuentra en la posición {posicion}.");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {numeroBuscar} no se encuentra en el arreglo.");
+            }
+        }
+
+        public void Actv6()
+        {
+            string[] cadena = { "Manzana", "Naranja", "Pera", "Uva", "Zanahoria" };
+
+            Console.WriteLine("Ingrese la cadena a buscar:");
+            string encontrar = Console.ReadLine();
+
+            int inicio = 0;
+            int fin = cadena.Length - 1;
+            int posicion = -1;
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+
+                if (cadena[medio] == encontrar)
+                {
+                    posicion = medio;
+                    break;
+                }
+                else if (string.Compare(cadena[medio], encontrar) < 0)
+                {
+                    inicio = medio + 1;
+                }
+                else
+                {
+                    fin = medio - 1;
+                }
+            }
+
+            if (posicion != -1)
+            {
+                Console.WriteLine($"La cadena '{encontrar}' se encuentra en la posición {posicion}.");
+            }
+            else
+            {
+                Console.WriteLine($"La cadena '{encontrar}' no se encuentra en el arreglo.");
+            }
+
+        }
+
+        public void Actv7()
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int inicio = 0;
+            int fin = num.Length - 1;
+            int numPar = -1;
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+
+                if (num[medio] % 2 == 0)
+                {
+                    if (medio == 0 || num[medio - 1] % 2 != 0)
                     {
-                        Console.WriteLine($"Fila {i}, Columna {j}: {matriz[i, j]}");
+                        numPar = medio;
+                        break;
+                    }
+                    else
+                    {
+                        fin = medio - 1; 
                     }
                 }
+                else
+                {
+                    inicio = medio + 1; 
+                }
+            }
+
+            if (numPar != -1)
+            {
+                Console.WriteLine("La posición del primer número par es: " + numPar);
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron números pares en el arreglo.");
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -6,61 +6,30 @@ namespace EjercicioBuscar_JSVE
     {
         static void Main(string[] args)
         {
-            int[] numeros = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 }; 
+            metodo metodo = new metodo();
+            metodo.Actv1();
 
-            Console.Write("Ingresa un número a buscar: ");
-            int numeroABuscar = Convert.ToInt32(Console.ReadLine());
+            metodo metodo2 = new metodo();
+            metodo2.Actv2();
 
-            int posicion = metodo.BuscarNumero(numeros, numeroABuscar);
+            metodo metodo3 = new metodo();
+            metodo3.Actv3();
 
-            if (posicion != -1)
-            {
-                Console.WriteLine($"El número {numeroABuscar} se encuentra en la posición {posicion} de la lista.");
-            }
-            else
-            {
-                Console.WriteLine($"El número {numeroABuscar} no se encuentra en la lista.");
-            }
+            metodo metodo4 = new metodo();
+            metodo4.Actv4();
 
-            string[] cadenas = { "Manzana", "Banana", "Cereza", "Damasco", "Fresa" }; 
+            metodo metodo5 = new metodo();
+            metodo5.Actv5();
 
-            Console.Write("Ingresa una cadena a buscar: ");
-            string cadenaABuscar = Console.ReadLine();
+            metodo metodo6 = new metodo();
+            metodo6.Actv6();
 
-            int indice = metodo.BuscarCadena(cadenas, cadenaABuscar);
+            metodo metodo7 = new metodo();
+            metodo7.Actv7();
 
-            if (indice != -1)
-            {
-                Console.WriteLine($"La cadena '{cadenaABuscar}' se encuentra en el índice {indice} del arreglo.");
-            }
-            else
-            {
-                Console.WriteLine($"La cadena '{cadenaABuscar}' no se encuentra en el arreglo.");
-            }
 
-            int[] numeros2 = { 3, 5, 7, 8, 11, 14, 9, 2, 6, 1 }; 
 
-            int indice2 = metodo.EncontrarPrimerNumeroPar(numeros2);
 
-            if (indice != -1)
-            {
-                Console.WriteLine($"El primer número par se encuentra en la posición {indice2} del arreglo.");
-            }
-            else
-            {
-                Console.WriteLine("No se encontró ningún número par en el arreglo.");
-            }
-           
-            static void Main(string[] args)
-            {
-                int[,] matriz = {
-                { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 }
-            };
-
-                metodo.EncontrarNumerosPares(matriz);
-            }
         }
     }
 }
